b1d4da57bcfcd8f5820bb54fa2d93a04d95e6a4d
Somkiat <somkiat.p@gmail.com>
2014-04-19 11:44:09 +0700

    Add if statement to check empty list

---
+++ learn006.erl
@@ -0,0 +1,23 @@
+-module(learn006).
+
+-export[average/1].
+
+sum_tail([], Acc) -> Acc;
+sum_tail([H|T], Acc) ->
+	sum_tail(T, H+Acc).
+
+count_tail([], Acc) -> Acc;
+count_tail([_|T], Acc) ->
+	count_tail(T, Acc+1).
+
+average(L) ->
+	if
+		L =/= [] ->
+			sum_tail(L, 0)/count_tail(L, 0);
+		true ->
+			error
+	end.
+	
+
+
+
